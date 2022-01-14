532a67261e2f9c22e8b071a2ed2cd8561b9359df
mkaraki <26180919+mkaraki@users.noreply.github.com>
2022-01-14 16:51:46 +0900

    Add text insert and batch processing

---
+++ process.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+while read row; do
+    inputfile=`echo ${row} | cut -d , -f 1`
+    if [[ "$inputfile" == "Input" ]]; then
+        continue
+    fi
+
+    filename=$(basename -- "$inputfile")
+    ext="${filename##*.}"
+
+    title=`echo ${row} | cut -d , -f 2`
+    information1=`echo ${row} | cut -d , -f 3`
+    information2=`echo ${row} | cut -d , -f 4`
+    startsec=`echo ${row} | cut -d , -f 5`
+    endsec=`echo ${row} | cut -d , -f 6`
+
+    outputfile=$inputfile.faded.$ext
+
+    ./gen_feded_video_with_text.sh "$startsec" "$endsec" "$title" "$information1" "$information2" "$inputfile" "$outputfile"
+
+done < $1
